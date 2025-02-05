9cd0d2f21c222b172e16bde5c0e9e9647f9bb8ee
CH258 <ch2953har@gmail.com>
2025-02-04 21:11:25 -0500

    add homepage details

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,11 +1,13 @@
 import Auth from '../utils/auth';
 import CoverPage from '../components/coverPage';
-
+import HomePanel from '../components/homePanel';
 const Homepage = () => {
     return (
         <>
             {Auth.loggedIn() ? (
-                <p>Logged In</p>
+                <>
+                    <HomePanel />
+                </>
             ) : (
                 <>
                     <CoverPage />
