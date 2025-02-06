62ffef5335d8698dd09ca690148a1e60463cfa12
Connor Dailey <connorldailey@gmail.com>
2025-02-06 10:28:21 -0500

    Update client side queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -10,9 +10,9 @@ export const GET_ME = gql`
     }
 `;
 
-export const GET_EXERCISES = gql`
-    query Exercises {
-        exercises {
+export const SEARCH_BY_NAME = gql`
+    query SearchByName($name: String!, $offset: Int) {
+        exercisesByName(name: $name, offset: $offset) {
             id
             name
             bodyPart
@@ -23,9 +23,9 @@ export const GET_EXERCISES = gql`
     }
 `;
 
-export const GET_EXERCISE = gql`
-    query Exercise($id: ID!) {
-        exercise(id: $id) {
+export const SEARCH_BY_BODYPART = gql`
+    query SearchByBodyPart($bodyPart: String!, $offset: Int) {
+        exercisesByBodyPart(bodyPart: $bodyPart, offset: $offset) {
             id
             name
             bodyPart
@@ -36,9 +36,66 @@ export const GET_EXERCISE = gql`
     }
 `;
 
-export const SEARCH_EXERCISES = gql`
-    query SearchExercises($query: String!) {
-        searchExercises(query: $query) {
+export const SEARCH_BY_EQUIPMENT = gql`
+    query SearchByEquipment($equipment: String!, $offset: Int) {
+        exercisesByEquipment(equipment: $equipment, offset: $offset) {
+            id
+            name
+            bodyPart
+            equipment
+            target
+            gifUrl
+        }
+    }
+`;
+
+export const SEARCH_BY_TARGET = gql`
+    query SearchByTarget($target: String!, $offset: Int) {
+        exercisesByTarget(target: $target, offset: $offset) {
+            id
+            name
+            bodyPart
+            equipment
+            target
+            gifUrl
+        }
+    }
+`;
+
+export const SEARCH_BY_ID = gql`
+    query SearchById($id: ID!) {
+        exerciseById(id: $id) {
+            id
+            name
+            bodyPart
+            equipment
+            target
+            gifUrl
+        }
+    }
+`;
+
+export const GET_BODYPART_LIST = gql`
+    query GetBodyPartsList {
+        bodyPartList 
+    }
+`;
+
+export const GET_EQUIPMENT_LIST = gql`
+    query GetEquipmentList {
+        equipmentList 
+    }
+`;
+
+export const GET_TARGET_LIST = gql`
+    query GetTargetList {
+        targetList 
+    }
+`;
+
+export const GET_RANDOM_EXERCISES = gql`
+    query GetRandomExercises {
+        randomExercises {
             id
             name
             bodyPart
