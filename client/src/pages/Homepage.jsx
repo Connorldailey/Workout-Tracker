cea75d0b226f784fe98fd8370defba0d770a4a6e
Maverick S <maverickmmsc@gmail.com>
2025-02-11 18:48:01 -0500

    lil clean up

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,17 +1,40 @@
 import Auth from '../utils/auth';
-import CoverPage from '../components/coverPage';
-import HomePanel from '../components/homePanel';
+import React from 'react';
+import backgroundImage from '../assets/—Pngtree—3d rendered gym equipment against_11966449.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
     return (
         <>
             {Auth.loggedIn() ? (
                 <>
-                    <HomePanel />
+                    <div>
+                        <p>Agenda of Workouts for the Week</p>
+                    </div>
+                    <Link to='/WorkoutLog'>
+                        <button className="button">Start Workout</button>
+                    </Link>
+                    <div>
+                        <p>Last 5 recorded workouts</p>
+                    </div>
                 </>
             ) : (
                 <>
-                    <CoverPage />
+                    <div
+                        className="d-flex justify-content-center align-items-center vh-100 bg-image"
+                        style={{
+                            backgroundImage: `url(${backgroundImage})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            imageRendering: 'crisp-edges'
+                        }}
+                    >
+                        <h1 className="text-white text-center fw-bold">
+                            Stay Active. Stay Strong. <br />
+                            Track your workouts with us! Just Log in or Signup!
+                        </h1>
+                    </div>
                 </>
             )}
         </>
